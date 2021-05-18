8d8b862c714fb242e261bc54875356ef99f82760
Ishrat Jahan <jahanishratme@gmail.com>
2021-05-18 21:46:00 +1000

    Added footer and navbar, updated styling

---
+++ footer.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="css/footer.css">
+</head>
+<body>
+   <div class="footer">
+   	<div class="inner_footer">
+   		
+   	</div>
+   </div>
+</body>
+</html>
---
+++ navbar.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="css/nav.css">
+</head>
+<body>
+   <div class="wrapper">
+   	  <nav>
+   	  	<div id="logo"><span id="first">HO</span><span id="second">BAN</span></div>
+   	  	<ul>
+   	  		<li><a href="index.php">Appointment</a></li>
+   	  		<li><a href="slots.php">Slot Setting</a></li>
+   	  		<li><a href="#">Student Login</a></li>
+            <li><a href="#">Organizer Login</a></li>
+   	  	</ul>
+   	  </nav>
+   </div>
+</body>
+</html>
